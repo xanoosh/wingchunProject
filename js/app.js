a2ff5bc2036d8cfb07748d8ff81b0ea80ca4914d
Janusz Bielczynski <xanoosh@gmail.com>
2018-06-20 14:02:16 +0200

    added form, dropdown menu and jquery script for scrolling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 
+$(function () {
 
+
+
+var anchor = $('ul,li,a');
+
+anchor.click(function (e) {
+    e.preventDefault();
+
+
+    var href = $(this).attr('href');
+
+    var section = $(href);
+
+    var height = section.offset().top;
+
+    var px = section.offset().top;
+
+    //$('html,body').scrollTop(px,0);
+
+    $('html,body').animate({'scrollTop':px},1500);
+
+});
+
+});
 
